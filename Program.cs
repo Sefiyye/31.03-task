c62a62f58c2532aeff76f24c427ab765eff211ad
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-31 09:21:02 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bycle bycle = new Bycle("Qara", "Cervelo", 1);
+            Car car = new Car(5, 5, 1, "Boz", "Acura", 200);
+
+            foreach (var item in bycle
+            {
+              
+            }
+            {
+
+            }
+        }
+    }
+}
